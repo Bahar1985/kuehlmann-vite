341fb3bf022e58f7d5b9bb5e2402f584d4b5991e
Sedigheh Balyavi <bayavisedigheh@gmail.com>
2023-08-09 15:04:01 +0200

    added images

--- src/App.ts
+++ src/App.ts
@@ -3,10 +3,11 @@ import { getMenu, getCurrentPage } from "./Router";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
 <section class="header">
-	<h1>Info Site</h1>
+
 	${getMenu()}
 </section>
 <main>
+
 	${getCurrentPage()}
 </main>
 `;
