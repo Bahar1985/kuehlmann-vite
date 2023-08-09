460a725c813a4c1ab283ee78b1095a77d5391135
Sedigheh Balyavi <bayavisedigheh@gmail.com>
2023-08-09 15:27:34 +0200

    added home-inhalt

--- src/App.ts
+++ src/App.ts
@@ -10,4 +10,33 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
 
 	${getCurrentPage()}
 </main>
+<!---------Footer------->
+<hr>
+<section class="footer">
+	<!-- <h4> About us</h4> -->
+	<!-- <p> Diplom Ingenieur und Architekt Lutz Kühlmann ist <br> der Kopf der Kühlmann Projektleitung und
+		Bauherrenvertretung.</p> -->
+	<div class="icons">
+		<i class="fa-brands fa-facebook"></i>
+		<i class="fa-brands fa-instagram"></i>
+		<i class="fa-brands fa-linkedin"></i>
+	</div>
+	<p>© 2023 Meine Website. Alle Rechte vorbehalten. | <a href="https://www.example.com">Impressum</a> | <a
+	 href="https://www.example.com/datenschutz">Datenschutz</a></p>
+	<p> Made With <i class="fa-regular fa-heart"></i> by Dci Student</p>
+</section>
+
+
+	<!-- <script--->
+<script>
+
+ const navLinks = document.getElementById("navLinks");
+ function showMenu() {
+	 navLinks.style.right = "0";
+ }
+ function hideMenu() {
+	 navLinks.style.right = "-200px";
+ }
+
+</script>
 `;
--- src/pages/PageHome.ts
+++ src/pages/PageHome.ts
@@ -1,7 +1,33 @@
 export const PageHome = () => {
 	return /*html*/`
 <div class="page pageHome">
-	<p>Welcome to this site.</p>
+<section class="bau-herren" id="home">
+<h1>Bauherrenvertretung (individuell vereinbar) </h1>
+<p>Besonders ist auch, dass der Projekteinstieg jederzeit möglich ist.</p>
+<div class="row">
+	<div class="bau-herren-col">
+		<h3> wort-1</h3>
+		<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis enim quod cumque eaque
+			quos!Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis enim quod cumque eaque
+			quos!
+		</p>
+	</div>
+	<div class="bau-herren-col">
+		<h3> wort-2</h3>class
+		<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis enim quod cumque eaque
+			quos!Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis enim quod cumque eaque
+			quos!
+		</p>
+	</div>
+	<div class="bau-herren-col">
+		<h3> wort-3</h3>
+		<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis enim quod cumque eaque
+			quos!Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis enim quod cumque eaque
+			quos!
+		</p>
+	</div>
+</div>
+</section>
 </div>
 `; 
 }
